d8f952366f7b87650559fcfcc12e51a3500044e9
keerthanak-tw <keerthana.kasthuril@gmail.com>
2023-03-25 23:09:26 +0530

    Github profile - Display dynamic user data

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -1,13 +1,50 @@
 const APIURL = 'https://api.github.com/users/';
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+const createUserCard = (user) => {
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+            </div>
+          <div class="user-info">
+            <h2>${user.name}</h2>
+            <p>${user.bio}</p>
+
+            <ul>
+                <li>${user.followers} <strong>Followers</strong></li>
+                <li>${user.following} <strong>Following</strong></li>
+                <li>${user.public_repos} <strong>Repos</strong></li>
+            </ul>
+
+            <div id="repos"></div>
+          </div>  
+        </div>
+    `;
+
+    main.innerHTML = cardHTML;
+}
+
+const createErrorCard = (message) => {
+    const cardHTML = `
+        <div class="card">
+            <h1>${message}</h1>
+        </div>
+    `;
+
+    main.innerHTML = cardHTML;
+}
 
 const getUser = async (username) => {
     try {
         const { data } = await axios(APIURL + username);
-        console.log(data);
+        createUserCard(data);
     } catch(error) {
-        console.log(err);
+        if(error.response.status === 404) {
+            createErrorCard('No profile with this username');
+        }
     }
 }
 
