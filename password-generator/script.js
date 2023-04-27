021431a2a1a87762ae91076e4ba7871ac1d4876a
keerthanak-tw <keerthana.kasthuril@gmail.com>
2023-04-27 21:32:48 +0530

    Random password generator

--- password-generator/script.js
+++ password-generator/script.js
@@ -1,23 +1,69 @@
 const getRandomLower = () => {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
 
 const getRandomUpper = () => {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
 
 const getRandomNumber = () => {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
 
 const getRandomSymbol = () => {
-    const symbols = '!@#$%^&*(){}[]=<>/,.';
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-const randomFunction = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol,
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 };
+
+const randomFunctionMap = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
+const generatePassword = (lower, upper, number, symbol, length) => {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+        const funcName = Object.keys(type)[0];
+        generatedPassword += randomFunctionMap[funcName]();
+    })
+  }
+  return generatedPassword.slice(0, length);
+};
+
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
